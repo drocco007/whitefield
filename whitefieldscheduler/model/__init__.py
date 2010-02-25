0217a91d4c64838dd6241943212bf8857bb79145
Daniel Rocco <drocco@gmail.com>
2010-02-25 18:37:51 -0500

    normal day schedule

--- whitefieldscheduler/model/__init__.py
+++ whitefieldscheduler/model/__init__.py
@@ -11,15 +11,24 @@ Licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Un
 http://creativecommons.org/licenses/by-nc-sa/3.0/us/
 
 
-For a given day code, the order of class periods is given here, so we 
-can  find the class period order with
+For a given day code, the order of class periods is given in day_period, 
+so we can find the class period order with
 
 >>> day_periods[day_code["8/21/2009"][0]]
 [1, 5, 6, 7, 2, 3]
+>>> day_periods[day_code["8/28/2009"][0]]
+[2, 3, 4, 5, 6, 7]
+
+The modifier tells us the schedule for the day: 
+
+>>> schedule[day_code["9/1/2009"][1]][0]
+('7:45-8:10', 'Extra Help')
+>>> schedule[day_code["8/31/2009"][1]][2]
+('8:30-9:25', 'Class')
 """
 
 """
-Encode a string date to a (date code, modifier) pair, either of which
+Encode a string date to a (day code, modifier) pair, either of which
 may be empty.
 """
 day_code = {
@@ -50,6 +59,21 @@ day_periods = {
     "G": [2, 3, 4, 5, 6, 7],
 }
 
+"""
+Define the schedule for a given modifier. 
+"""
+schedule = {
+    "": [("7:45-8:10", "Extra Help"), 
+         ("8:10-8:25", "Advisee"),
+         ("8:30-9:25", "Class"),
+         ("9:30-10:25", "Class"),
+         ("10:30-11:25", "Class"),
+         ("11:30-12:25", "Class"),
+         ("12:30-1:00", "Lunch"),
+         ("1:05-2:00", "Class"),
+         ("2:05-3:00", "Class"),
+         ],
+}
 
 # ====================================================================
 # doctest test harness
