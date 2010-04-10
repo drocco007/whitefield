f2a0fd0e6a0b91fa7c5c5d8164a3d580b92995eb
Daniel Rocco <drocco@gmail.com>
2010-04-10 00:15:30 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='WhitefieldScheduler',
-    version='0.2',
+    version='0.3',
     description='Whitefield class schedule application',
     author='Daniel J. Rocco',
     author_email='drocco@gmail.com',
