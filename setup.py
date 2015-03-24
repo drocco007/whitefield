bea051d05bbae68039129434959617e13294ae48
Daniel Rocco <drocco@gmail.com>
2015-03-23 23:14:37 -0400

    Database infrastructure: psycopg2, SQLA

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
 
 
+requires = [
+    'pyramid',
+    'pyramid_tm',
+    'SQLAlchemy',
+    # 'psycopg2',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'parsedatetime>=0.8.6',
+]
+
 setup(
     name='WhitefieldScheduler',
     version='0.10.2',
@@ -8,16 +19,10 @@ setup(
     author='Daniel J. Rocco',
     author_email='drocco@gmail.com',
     url='',
-    install_requires=[
-        'parsedatetime>=0.8.6',
-        'pyramid',
-        'waitress',
-    ],
+    install_requires=requires,
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
-    package_data={'whitefieldscheduler': ['i18n/*/LC_MESSAGES/*.mo']},
     zip_safe=False,
-    paster_plugins=['PasteScript', 'Pylons'],
     entry_points="""
     [paste.app_factory]
     main = whitefield.mobile.__main__:main
