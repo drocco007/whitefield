5dcb2c20a4e99f7a16e2a3259000ff11c40feb52
Daniel Rocco <drocco@gmail.com>
2015-03-29 22:09:27 -0400

    Nicer menus, logout handling

--- whitefield/mobile/static/whitefield_auth.js
+++ whitefield/mobile/static/whitefield_auth.js
@@ -1,33 +1,67 @@
-var SignInControl = React.createClass({
-    render: function() {
-        return (
-            <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    <i className="glyphicon glyphicon-user"></i>
-                    Sign in
-                </a>
+var SignInOutControl = React.createClass({
+    logout: function(e) {
+        $(this.getDOMNode()).parents(".collapse").collapse("hide");
 
-                <form className="dropdown-menu" action="/1/auth/login" method="post">
-                    <div className="form-group">
-                        <label htmlFor="email">Email address</label>
-                        <input type="email" className="form-control" id="email"
-                            name="email" placeholder="Email address" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password"
-                            name="password" placeholder="Password" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                    <a href="/register.html" className="col-xs-offset-2">Create new account</a>
-                </form>
-            </li>
-        );
+        $.get('/1/auth/logout', function() {
+            this.props.onLogout();
+        }.bind(this));
+    },
+
+    render: function() {
+        if (this.props.full_name) {
+            return (
+                <li>
+                    <a href="#" onClick={this.logout}>Sign out</a>
+                </li>
+            );
+        } else {
+            return (
+                <li className="dropdown">
+                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <i className="glyphicon glyphicon-user"></i>
+                        Sign in
+                    </a>
+
+                    <form className="dropdown-menu" action="/1/auth/login" method="post">
+                        <div className="form-group">
+                            <label htmlFor="email">Email address</label>
+                            <input type="email" className="form-control" id="email"
+                                name="email" placeholder="Email address" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" id="password"
+                                name="password" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <a href="/register.html" className="col-xs-offset-2">Create new account</a>
+                    </form>
+                </li>
+            );
+        }
     }
 });
 
 
-var AuthControl = React.createClass({
+var ProfileControl = React.createClass({
+    render: function() {
+        if (this.props.full_name) {
+            return (
+                <li className="">
+                    <a href="#">
+                        <i className="glyphicon glyphicon-user"></i>&nbsp;
+                        {this.props.full_name}
+                    </a>
+                </li>
+            );
+        } else {
+            return null;
+        }
+    }
+});
+
+
+var NavBar = React.createClass({
     getInitialState: function() {
         return {};
     },
@@ -37,7 +71,7 @@ var AuthControl = React.createClass({
             url: '/1/auth/users/current',
             dataType: 'json',
             success: function (data) {
-                this.setState(data);
+                this.replaceState(data);
             }.bind(this),
             error: function (xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -50,39 +84,16 @@ var AuthControl = React.createClass({
     },
 
     render: function() {
-        if (!this.state.full_name) {
-            return (
-                <SignInControl />
-            );
-        } else {
-            // FIXME: figure out a way to make both React and Bootstrap happy…
-            return (
-                <div>
-                    <li className="">
-                        <a href="#">
-                            <i className="glyphicon glyphicon-user"></i>&nbsp;
-                            {this.state.full_name}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/1/auth/logout">Sign out</a>
-                    </li>
-                </div>
-            );
-        }
-    }
-});
-
-
-var NavBar = React.createClass({
-    render: function() {
         return (
             <ul className="nav navbar-nav navbar-right">
-                <AuthControl />
+                <ProfileControl full_name={this.state.full_name} />
+                <SignInOutControl full_name={this.state.full_name}
+                    onLogout={this.loadUser} />
             </ul>
         );
     }
 });
+
 
 React.render(
     <NavBar />,
